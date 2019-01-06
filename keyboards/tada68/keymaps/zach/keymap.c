e8f91df1093474ef7e807a3fe4a00eec4f312fa9
Zach Marshall <Zach.Marshall@sas.com>
2019-01-05 23:59:40 -0500

    Toggles backlight when receiving report

--- keyboards/tada68/keymaps/zach/keymap.c
+++ keyboards/tada68/keymaps/zach/keymap.c
@@ -12,4 +12,5 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 void process_console_data_user(uint8_t * data, uint8_t length) {
     println("In user data for zach");
     dprintln("In user data for zach");
+    backlight_toggle();
 }
